068532ff48c119733be6110b3673ade6887463fc
linstack <1185530689@qq.com>
2022-09-30 23:24:52 +0800

    新建Person类，定义了基本属性

---
+++ TestModule01/src/com/linstack/Person.java
@@ -0,0 +1,11 @@
+package com.linstack;
+
+/**
+ * @Auther: LinStack
+ * @Date: 2022/9/30 - 09 - 30
+ * @Description: com.linstack
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
